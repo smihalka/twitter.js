b98c44eeae7e464ee7f4cdc884ba0a6c9c52e5c0
Lina Jones <lina.jones630@gmail.com>
2017-06-13 10:58:02 -0600

    updated layouts, css, and app.js to remove gets

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require( 'express' );
 const chalk = require( 'chalk' );
 const nunjucks = require('nunjucks');
 const app = express(); // creates an instance of an express application
+const routes = require('./routes');
 
 app.set('view engine', 'html'); // have res.render work with html files
 app.engine('html', nunjucks.render); // when giving html files to res.render, tell it to use nunjucks
@@ -20,6 +21,9 @@ nunjucks.configure('views',{noCache: true});
 // nunjucks.render('index.html', locals, function (err, output) {
 //     console.log(output);
 // });
+app.use('/', routes);
+
+app.use(express.static('public'))
 
 app.use(function (req, res, next) {
   console.log('Time:', Date.now());
@@ -28,24 +32,24 @@ app.use(function (req, res, next) {
     next();
 })
 
-app.get('/', function(req, res) {
-  // res.send('this is a sample!');
+// app.get('/', function(req, res) {
+//   // res.send('this is a sample!');
 
-  res.render( 'index', {title: locals.title , people: locals.people, pGraph: locals.pGraph} );
-  // res.render(nunjucks.render('index.html', locals, function (err, output) {
-  //   console.log(output);
-  // }));
-});
+//   res.render( 'index', {title: locals.title , people: locals.people, pGraph: locals.pGraph} );
+//   // res.render(nunjucks.render('index.html', locals, function (err, output) {
+//   //   console.log(output);
+//   // }));
+// });
 
-app.get('/is-anybody-in-there', function(req, res) {
-  res.send('this is someone else');
-});
-app.get('/modernism', function (req, res) {
-  res.send('this is the get')
-})
-app.post('/modernism', function (req, res) {
-  res.send('POST request to the homepage')
-})
+// app.get('/is-anybody-in-there', function(req, res) {
+//   res.send('this is someone else');
+// });
+// app.get('/modernism', function (req, res) {
+//   res.send('this is the get')
+// })
+// app.post('/modernism', function (req, res) {
+//   res.send('POST request to the homepage')
+// })
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
--- tweetBank.js
+++ tweetBank.js
@@ -31,13 +31,8 @@ const getFakeTweet = function() {
   return "Fullstack Academy is " + randArrayEl(awesome_adj) + "! The instructors are just so " + randArrayEl(awesome_adj) + ". #fullstacklove #codedreams";
 };
 
-//for (let i = 0; i < 10; i++) {
-  module.exports.add( "jing","Today is too HOT!!!!" );
-//}
+for (let i = 0; i < 10; i++) {
+  module.exports.add( getFakeName(), getFakeTweet() );
+}
 
-//add("jing","Today is too HOT!!!!")
-
-console.log(find(data,(name)=>{
-  return data.name === "jing";
-}));
-//console.log(data)
+console.log(data);
