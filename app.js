bb3c254f7595367f3c994821f93ba80115d593cf
Shayne Mihalka <shayne@mihalka.com>
2017-06-13 12:38:42 -0700

    best team ever

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const chalk = require( 'chalk' );
 const nunjucks = require('nunjucks');
 const app = express(); // creates an instance of an express application
 const routes = require('./routes');
+const bodyParser = require('body-parser');
 
 app.set('view engine', 'html'); // have res.render work with html files
 app.engine('html', nunjucks.render); // when giving html files to res.render, tell it to use nunjucks
@@ -21,6 +22,9 @@ nunjucks.configure('views',{noCache: true});
 // nunjucks.render('index.html', locals, function (err, output) {
 //     console.log(output);
 // });
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded())
 app.use('/', routes);
 
 app.use(express.static('public'))
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,10 @@
 const express = require('express');
+
 const router = express.Router();
 // could use one line instead: const router = require('express').Router();
 const tweetBank = require('../tweetBank');
+
+//res.render( 'index', { tweets: tweets, showForm: true } );
 
 router.get('/users/:name', function(req, res) {
   var name = req.params.name;
@@ -12,9 +15,15 @@ router.get('/users/:name', function(req, res) {
 
 router.get('/', function (req, res) {
   let tweets = tweetBank.list();
-  res.render( 'index', { tweets: tweets } );
+  res.render( 'index', { tweets: tweets, showForm: true } );
 });
 
+router.post('/tweets', function(req, res) {
+  var name = req.body.name;
+  var text = req.body.text;
+  tweetBank.add(name, text);
+  res.redirect('/');
+});
 
 
 module.exports = router;
